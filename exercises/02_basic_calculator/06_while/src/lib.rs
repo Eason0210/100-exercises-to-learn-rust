e4f301d2d03537c8fc3aa27cdde89187beba363d
Eason0210 <aqua0210@163.com>
2025-08-02 17:58:51 +0800

    Refactor while loop

--- exercises/02_basic_calculator/06_while/src/lib.rs
+++ exercises/02_basic_calculator/06_while/src/lib.rs
@@ -1,21 +1,12 @@
 // Rewrite the factorial function using a `while` loop.
 pub fn factorial(n: u32) -> u32 {
-    // The `todo!()` macro is a placeholder that the compiler
-    // interprets as "I'll get back to this later", thus
-    // suppressing type errors.
-    // It panics at runtime.
-    if (n == 0 || n == 1) {
-        1
-    } else {
-        let mut res = n;
-        let mut i = n - 1;
-
-        while i > 0 {
-            res *= i;
-            i = i - 1;
-        }
-        res
+    let mut res = 1;
+    let mut i = 1;
+    while i <= n {
+        res *= i;
+        i += 1;
     }
+    res
 }
 
 #[cfg(test)]
