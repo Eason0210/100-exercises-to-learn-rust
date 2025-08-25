ff135f4fd6749ff774f1a9d508f24d7267ab2219
Eason0210 <aqua0210@163.com>
2025-08-25 22:30:17 +0800

    Add TODO

--- exercises/05_ticket_v2/07_unwrap/src/lib.rs
+++ exercises/05_ticket_v2/07_unwrap/src/lib.rs
@@ -6,6 +6,7 @@ fn easy_ticket(title: String, description: String, status: Status) -> Ticket {
     match ticket {
         Ok(t) => t,
         Err(e) => {
+            // TODO: use e.contains instead
             if e == "Title cannot be empty" {
                 panic!("{}", e);
             } else if e == "Title cannot be longer than 50 bytes" {
