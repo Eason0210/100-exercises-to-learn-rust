48b54c74e5f9f9c2961282bcc57d733374fb3051
Eason0210 <aqua0210@163.com>
2025-08-31 16:27:11 +0800

    Fix 5.12 thiserror: No need to impl Display for TicketNewError

--- exercises/05_ticket_v2/12_thiserror/src/lib.rs
+++ exercises/05_ticket_v2/12_thiserror/src/lib.rs
@@ -6,25 +6,14 @@ use std::fmt::Display;
 //   You'll also have to add `thiserror` as a dependency in the `Cargo.toml` file.
 #[derive(Debug, thiserror::Error)]
 enum TicketNewError {
+    #[error ("Title cannot be empty")]
     TitleCannotBeEmpty,
+    #[error ("Title cannot be longer than 50 bytes")]
     TitleTooLong,
+    #[error ("Description cannot be empty")]
     DescriptionCannotBeEmpty,
+    #[error ("Description cannot be longer than 500 bytes")]
     DescriptionTooLong,
-}
-
-impl Display for TicketNewError {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            TicketNewError::TitleCannotBeEmpty => write!(f, "Title cannot be empty"),
-            TicketNewError::TitleTooLong => write!(f, "Title cannot be longer than 50 bytes"),
-            TicketNewError::DescriptionCannotBeEmpty => {
-                write!(f, "Description cannot be empty")
-            }
-            TicketNewError::DescriptionTooLong => {
-                write!(f, "Description cannot be longer than 500 bytes")
-            }
-        }
-    }
 }
 
 #[derive(Debug, PartialEq, Clone)]
